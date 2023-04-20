b9f67abf0a4322d3a63bbfeae9c4fa942f70cfa2
Eduardo Celino <ehcelino@gmail.com>
2023-04-20 18:49:03 -0300

    Mensagem flash adicionada

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,25 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+    @warehouse = Warehouse.new
+  end
+
+  def create
+    @warehouse = Warehouse.new(warehouse_params)
+    if @warehouse.save
+      redirect_to root_path, notice: "Galpão cadastrado com sucesso."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :description,
+                                      :code, :address, :city, :cep, :area)
+  end
+
 end
--- spec/system/user_register_warehouse_spec.rb
+++ spec/system/user_register_warehouse_spec.rb
@@ -18,4 +18,29 @@ describe 'Usuário cadastra um galpão' do
     expect(page).to have_field('Área')
 
   end
+
+  it 'Com sucesso' do
+    # Arrange
+
+    # Act
+    visit root_path
+    click_on 'Cadastrar Galpão'
+    fill_in 'Nome', with: 'Rio de Janeiro'
+    fill_in 'Descrição', with: 'Galpão da zona portuária do Rio'
+    fill_in 'Código', with: 'RIO'
+    fill_in 'Endereço', with: 'Avenida do Museu do Amanhã, 1000'
+    fill_in 'Cidade', with: 'Rio de Janeiro'
+    fill_in 'CEP', with: '20100-000'
+    fill_in 'Área', with: '32000'
+    click_on 'Enviar'
+
+    # Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content('Galpão cadastrado com sucesso.')
+    expect(page).to have_content('Rio de Janeiro')
+    expect(page).to have_content('RIO')
+    expect(page).to have_content('32000 m2')
+
+  end
+
 end

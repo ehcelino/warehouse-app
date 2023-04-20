09406455a8b881df9810632b4b8f047e950c2b82
Eduardo Celino <ehcelino@gmail.com>
2023-04-19 21:26:21 -0300

    usuário vê os dados do galpão e volta para o root_path

--- spec/system/user_view_warehouse_details_spec.rb
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -8,7 +8,7 @@ describe 'Usuário vê detalhes de um galpão' do
                       description: 'Galpão destinado para cargas internacionais')
     w.save()
     # Act
-    visit('/')
+    visit(root_path)
     click_on 'Aeroporto SP'
 
     # Assert
@@ -19,6 +19,20 @@ describe 'Usuário vê detalhes de um galpão' do
     expect(page).to have_content('Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000')
     expect(page).to have_content('Galpão destinado para cargas internacionais')
 
+
+  end
+
+  it 'e volta para a tela inicial' do
+    # Arrange
+    w = Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+      address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+      description: 'Galpão destinado para cargas internacionais')
+    # Act
+      visit root_path
+      click_on 'Aeroporto SP'
+      click_on 'Voltar'
+    # Assert
+    expect(current_path).to eq(root_path)
 
   end
 end

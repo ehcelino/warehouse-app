a46c76fd72f7d720ac5e062972b92ef574320a07
Eduardo Celino <ehcelino@gmail.com>
2023-04-23 18:17:21 -0300

    devise e login

--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ProductModel, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it 'name obrigatório' do
+      # Arrange
+      supplier = Supplier.create!(corporate_name: 'Samsung Eletrônicos LTDA', brand_name: 'Samsung', registration_number: '1234567890123',
+        full_address: 'Av Nações Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+      pm = ProductModel.new(name: '', weight: 8000, width: 70, height: 45, depth: 10, sku: 'TV32-SAMSU-XPTO90', supplier: supplier)        
+
+      # Act
+      result = pm.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+  end
 end
--- spec/system/user_register_product_model_spec.rb
+++ spec/system/user_register_product_model_spec.rb
@@ -5,6 +5,8 @@ describe 'Usuário cadastra um modelo de produto' do
     # Arrange
     supplier = Supplier.create!(corporate_name: 'Samsung Eletrônicos LTDA', brand_name: 'Samsung', registration_number: '1234567890123',
                                 full_address: 'Av Nações Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+    other_supplier = Supplier.create!(corporate_name: 'LG Eletrônicos LTDA', brand_name: 'LG', registration_number: '1234567890123',
+      full_address: 'Av Ibirapuera, 1000', city: 'São Paulo', state: 'SP', email: 'sac@lg.com.br')
     # Act
     visit root_path
     click_on 'Modelos de Produtos'
@@ -25,5 +27,27 @@ describe 'Usuário cadastra um modelo de produto' do
     expect(page).to have_content('SKU: TV32-SAMSU-XPTO90')
     expect(page).to have_content('Dimensão: 70 x 45 x 10 cm')
     expect(page).to have_content('Peso: 8000g')
+  end
+
+  it 'deve preencher todos os campos' do
+    # Arrange
+    supplier = Supplier.create!(corporate_name: 'Samsung Eletrônicos LTDA', brand_name: 'Samsung', registration_number: '1234567890123',
+      full_address: 'Av Nações Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+    other_supplier = Supplier.create!(corporate_name: 'LG Eletrônicos LTDA', brand_name: 'LG', registration_number: '1234567890123',
+      full_address: 'Av Ibirapuera, 1000', city: 'São Paulo', state: 'SP', email: 'sac@lg.com.br')
+    # Act
+    visit root_path
+    click_on 'Modelos de Produtos'
+    click_on 'Cadastrar Novo'
+    fill_in 'Nome', with: ''
+    fill_in 'Peso', with: 8000
+    fill_in 'Largura', with: 70
+    fill_in 'Altura', with: 45
+    fill_in 'Profundidade', with: 10
+    fill_in 'SKU', with: ''
+    # select 'Samsung', from: 'Fornecedor'
+    click_on 'Enviar'
+    # Assert
+    expect(page).to have_content('Não foi possivel cadastrar o modelo de produto.')
   end
 end

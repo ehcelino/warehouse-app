478809566a9e902ddd2726dec84cc620c1c3e4c2
Eduardo Celino <ehcelino@gmail.com>
2023-04-26 22:46:49 -0300

    adequando cadastro de produtos

--- spec/system/product_models/user_register_product_model_spec.rb
+++ spec/system/product_models/user_register_product_model_spec.rb
@@ -18,7 +18,7 @@ describe 'Usuário cadastra um modelo de produto' do
     fill_in 'Largura', with: 70
     fill_in 'Altura', with: 45
     fill_in 'Profundidade', with: 10
-    fill_in 'SKU', with: 'TV32-SAMSU-XPTO90'
+    fill_in 'SKU', with: 'TV32-SAMSUNG-XPT3009'
     select 'Samsung', from: 'Fornecedor'
     click_on 'Enviar'
 
@@ -26,7 +26,7 @@ describe 'Usuário cadastra um modelo de produto' do
     expect(page).to have_content('Modelo de Produto cadastrado com sucesso.')
     expect(page).to have_content('TV 32')
     expect(page).to have_content('Fornecedor: Samsung')
-    expect(page).to have_content('SKU: TV32-SAMSU-XPTO90')
+    expect(page).to have_content('SKU: TV32-SAMSUNG-XPT3009')
     expect(page).to have_content('Dimensão: 70 x 45 x 10 cm')
     expect(page).to have_content('Peso: 8000g')
   end
@@ -49,9 +49,63 @@ describe 'Usuário cadastra um modelo de produto' do
     fill_in 'Altura', with: 45
     fill_in 'Profundidade', with: 10
     fill_in 'SKU', with: ''
-    # select 'Samsung', from: 'Fornecedor'
+    select 'Samsung', from: 'Fornecedor'
     click_on 'Enviar'
     # Assert
     expect(page).to have_content('Não foi possivel cadastrar o modelo de produto.')
   end
+
+  it 'e preenche o campo código com tamanho inválido' do
+    # Arrange
+    user = User.create!(name:'João', email: 'joao@email.com', password: 'password')
+    supplier = Supplier.create!(corporate_name: 'Samsung Eletrônicos LTDA', brand_name: 'Samsung', registration_number: '1234567890123',
+      full_address: 'Av Nações Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+    other_supplier = Supplier.create!(corporate_name: 'LG Eletrônicos LTDA', brand_name: 'LG', registration_number: '1234567890321',
+      full_address: 'Av Ibirapuera, 1000', city: 'São Paulo', state: 'SP', email: 'sac@lg.com.br')
+    # Act
+    login_as(user)
+    visit root_path
+    click_on 'Modelos de Produtos'
+    click_on 'Cadastrar Novo'
+    fill_in 'Nome', with: 'TV 32'
+    fill_in 'Peso', with: 8000
+    fill_in 'Largura', with: 70
+    fill_in 'Altura', with: 45
+    fill_in 'Profundidade', with: 10
+    fill_in 'SKU', with: 'TV32-SAMSUNG-XPT300'
+    select 'Samsung', from: 'Fornecedor'
+    click_on 'Enviar'
+    # Assert
+    expect(page).to have_content('Não foi possivel cadastrar o modelo de produto.')
+    expect(page).to have_content('SKU não possui o tamanho esperado (20 caracteres)')
+  end
+
+  it 'e preenche os campos de dimensões com valores inválidos' do
+    # Arrange
+    user = User.create!(name:'João', email: 'joao@email.com', password: 'password')
+    supplier = Supplier.create!(corporate_name: 'Samsung Eletrônicos LTDA', brand_name: 'Samsung', registration_number: '1234567890123',
+      full_address: 'Av Nações Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+    other_supplier = Supplier.create!(corporate_name: 'LG Eletrônicos LTDA', brand_name: 'LG', registration_number: '1234567890321',
+      full_address: 'Av Ibirapuera, 1000', city: 'São Paulo', state: 'SP', email: 'sac@lg.com.br')
+    # Act
+    login_as(user)
+    visit root_path
+    click_on 'Modelos de Produtos'
+    click_on 'Cadastrar Novo'
+    fill_in 'Nome', with: 'TV 32'
+    fill_in 'Peso', with: 0
+    fill_in 'Largura', with: 0
+    fill_in 'Altura', with: 0
+    fill_in 'Profundidade', with: 0
+    fill_in 'SKU', with: 'TV32-SAMSUNG-XPT3009'
+    select 'Samsung', from: 'Fornecedor'
+    click_on 'Enviar'
+    # Assert
+    expect(page).to have_content('Não foi possivel cadastrar o modelo de produto.')
+    expect(page).to have_content('Peso deve ser maior que 0')
+    expect(page).to have_content('Largura deve ser maior que 0')
+    expect(page).to have_content('Altura deve ser maior que 0')
+    expect(page).to have_content('Profundidade deve ser maior que 0')
+  end
+
 end
--- spec/system/product_models/user_view_product_models_spec.rb
+++ spec/system/product_models/user_view_product_models_spec.rb
@@ -33,9 +33,9 @@ describe 'Usuário vê modelos de produtos' do
     supplier = Supplier.create!(corporate_name: 'Samsung Eletrônicos LTDA', brand_name: 'Samsung', registration_number: '1234567890123',
                                  full_address: 'Av Nações Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
     ProductModel.create!(name: 'TV 32', weight: 8000, width: 70, height: 45, depth: 10,
-                        sku: 'TV32-SAMSU-XPTO90', supplier: supplier)
+                        sku: 'TV32-SAMSUNG-XPT3009', supplier: supplier)
     ProductModel.create!(name: 'SoundBar 7.1 Surround', weight: 3000, width: 80, height: 15, depth: 20,
-                        sku: 'SOU71-SAMSU-NOI277', supplier: supplier)
+                        sku: 'SOUND71-SAMSUNG-NOI2', supplier: supplier)
 
     # Act
     login_as(user)
@@ -47,10 +47,10 @@ describe 'Usuário vê modelos de produtos' do
     # Assert
     expect(current_path).to eq product_models_path
     expect(page).to have_content('TV 32')
-    expect(page).to have_content('TV32-SAMSU-XPTO90')
+    expect(page).to have_content('TV32-SAMSUNG-XPT3009')
     expect(page).to have_content('Samsung')
     expect(page).to have_content('SoundBar 7.1 Surround')
-    expect(page).to have_content('SOU71-SAMSU-NOI277')
+    expect(page).to have_content('SOUND71-SAMSUNG-NOI2')
     expect(page).to have_content('Samsung')
   end
 
@@ -68,5 +68,5 @@ describe 'Usuário vê modelos de produtos' do
     expect(current_path).to eq product_models_path
     expect(page).to have_content('Nenhum modelo de produto cadastrado.')
   end
-  
+
 end

9682155a4817e0edef96eeb9802f92512a97c4e4
Eduardo Celino <ehcelino@gmail.com>
2023-04-28 23:26:04 -0300

    entrada de produtos no estoque

--- spec/system/orders/user_update_order_status_spec.rb
+++ spec/system/orders/user_update_order_status_spec.rb
@@ -10,9 +10,10 @@ describe 'Usuário informa novo status de pedido' do
       description: 'Galpão destinado para cargas internacionais')
     supplier = Supplier.create!(corporate_name: 'Amazonas LTDA', brand_name: 'Amazonas', registration_number: '1234567890123',
                         full_address: 'Av Central, 1000', city: 'Manaus', state: 'AM', email: 'comercial@amazonas.com')
-
+    product = ProductModel.create!(supplier: supplier, name: 'Cadeira Gamer', weight: 5000, height: 100, width: 70, depth: 75, sku: 'CADEIRA-09112-XPTO01')
     formatted_date = I18n.localize(1.day.from_now.to_date)
     order = Order.create!(user: joao, warehouse: warehouse, supplier: supplier, estimated_delivery_date: formatted_date, status: :pending)
+    OrderItem.create!(order: order, product_model: product, quantity: 5)
 
     # Act
     login_as(joao)
@@ -26,8 +27,9 @@ describe 'Usuário informa novo status de pedido' do
     expect(page).to have_content('Situação do Pedido: Entregue')
     expect(page).not_to have_button('Marcar como CANCELADO')
     expect(page).not_to have_button('Marcar como ENTREGUE')
-
-
+    expect(StockProduct.count).to eq 5
+    stock = StockProduct.where(product_model: product, warehouse: warehouse).count
+    expect(stock).to eq 5
   end
 
   it 'e pedido foi cancelado' do
@@ -38,9 +40,10 @@ describe 'Usuário informa novo status de pedido' do
       description: 'Galpão destinado para cargas internacionais')
     supplier = Supplier.create!(corporate_name: 'Amazonas LTDA', brand_name: 'Amazonas', registration_number: '1234567890123',
                         full_address: 'Av Central, 1000', city: 'Manaus', state: 'AM', email: 'comercial@amazonas.com')
-
+    product = ProductModel.create!(supplier: supplier, name: 'Cadeira Gamer', weight: 5000, height: 100, width: 70, depth: 75, sku: 'CADEIRA-09112-XPTO01')
     formatted_date = I18n.localize(1.day.from_now.to_date)
     order = Order.create!(user: joao, warehouse: warehouse, supplier: supplier, estimated_delivery_date: formatted_date, status: :pending)
+    OrderItem.create!(order: order, product_model: product, quantity: 5)
 
     # Act
     login_as(joao)
@@ -52,7 +55,7 @@ describe 'Usuário informa novo status de pedido' do
     # Assert
     expect(current_path).to eq order_path(order.id)
     expect(page).to have_content('Situação do Pedido: Cancelado')
-
+    expect(StockProduct.count).to eq 0
   end
 
 end

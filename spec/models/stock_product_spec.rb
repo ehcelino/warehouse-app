ce8bb4f785f94924b5dd9f7394aa44c993d3818b
Eduardo Celino <ehcelino@gmail.com>
2023-04-29 17:26:39 -0300

    saída de itens do estoque

--- spec/models/stock_product_spec.rb
+++ spec/models/stock_product_spec.rb
@@ -43,4 +43,44 @@ RSpec.describe StockProduct, type: :model do
       expect(stock_product.serial_number).to eq original_serial_number
     end
   end
+
+  describe '#available?' do
+
+    it 'true se não tiver destino' do
+      # Arrange
+      warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', state: 'SP', area: 100_000,
+        address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+        description: 'Galpão destinado para cargas internacionais')
+      user = User.create!(name:'Sergio', email: 'sergio@email.com', password: '12345678')
+      supplier = Supplier.create!(corporate_name: 'Amazonas LTDA', brand_name: 'Amazonas', registration_number: '1234567890123',
+            full_address: 'Av Central, 1000', city: 'Manaus', state: 'AM', email: 'comercial@amazonas.com')
+      order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, status: :delivered, estimated_delivery_date: 1.week.from_now)
+      product = ProductModel.create!(name: 'Produto A', weight: 15, width: 10, height: 20, depth: 30, supplier: supplier, sku: 12345678912345678900)
+      # Act
+      stock_product = StockProduct.create!(order: order, warehouse: warehouse, product_model: product)
+
+
+      # Assert
+      expect(stock_product.available?).to eq true
+    end
+
+    it 'false se tiver destino' do
+      # Arrange
+      warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', state: 'SP', area: 100_000,
+        address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+        description: 'Galpão destinado para cargas internacionais')
+      user = User.create!(name:'Sergio', email: 'sergio@email.com', password: '12345678')
+      supplier = Supplier.create!(corporate_name: 'Amazonas LTDA', brand_name: 'Amazonas', registration_number: '1234567890123',
+            full_address: 'Av Central, 1000', city: 'Manaus', state: 'AM', email: 'comercial@amazonas.com')
+      order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, status: :delivered, estimated_delivery_date: 1.week.from_now)
+      product = ProductModel.create!(name: 'Produto A', weight: 15, width: 10, height: 20, depth: 30, supplier: supplier, sku: 12345678912345678900)
+      # Act
+      stock_product = StockProduct.create!(order: order, warehouse: warehouse, product_model: product)
+      stock_product.create_stock_product_destination!(recipient: 'João', address: 'Rua Tal')
+
+      # Assert
+      expect(stock_product.available?).to eq false
+
+    end
+  end
 end
--- spec/system/product_stock/user_view_stock_from_warehouse_spec.rb
+++ spec/system/product_stock/user_view_stock_from_warehouse_spec.rb
@@ -25,9 +25,41 @@ describe 'Usuário vê o estoque' do
     click_on('Aeroporto SP')
 
     # Assert
-    expect(page).to have_content 'Itens em Estoque'
-    expect(page).to have_content '3 x TV32-SAMSUNG-XPT3009'
-    expect(page).to have_content '2 x NOTE-SAMSUNG-XPTO20'
-    expect(page).not_to have_content 'SOUND71-SAMSUNG-NOI2'
+    within("section#stock_products") do
+      expect(page).to have_content 'Itens em Estoque'
+      expect(page).to have_content '3 x TV32-SAMSUNG-XPT3009'
+      expect(page).to have_content '2 x NOTE-SAMSUNG-XPTO20'
+      expect(page).not_to have_content 'SOUND71-SAMSUNG-NOI2'
+    end
   end
+
+  it 'e dá baixa em um item' do
+    # Arrange
+    user = User.create!(name:'João', email: 'joao@email.com', password: 'password')
+    w = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+      address: 'Avenida do Aeroporto, 1000', cep: '15000-000', state: 'SP',
+      description: 'Galpão destinado para cargas internacionais')
+    supplier = Supplier.create!(corporate_name: 'Samsung Eletrônicos LTDA', brand_name: 'Samsung', registration_number: '1234567890123',
+      full_address: 'Av Nações Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+    order = Order.create!(user: user, supplier: supplier, warehouse: w, estimated_delivery_date: 1.day.from_now)
+    product_tv = ProductModel.create!(name: 'TV 32', weight: 8000, width: 70, height: 45, depth: 10,
+                                      sku: 'TV32-SAMSUNG-XPT3009', supplier: supplier)
+    2.times { StockProduct.create!(order: order, warehouse: w, product_model: product_tv) }
+
+    # Act
+    login_as user
+    visit root_path
+    click_on 'Aeroporto SP'
+    select 'TV32-SAMSUNG-XPT3009', from: 'Item para Saída'
+    fill_in 'Destinatário', with: 'Maria Ferreira'
+    fill_in 'Endereço Destino', with: 'Rua das Palmeiras, 100 - Campinas - São Paulo'
+    click_on 'Confirmar Retirada'
+
+    # Assert
+    expect(current_path).to eq warehouse_path(w.id)
+    expect(page).to have_content 'Item retirado com sucesso'
+    expect(page).to have_content '1 x TV32-SAMSUNG-XPT3009'
+
+  end
+
 end
